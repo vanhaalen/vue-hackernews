6cbac486c9e2510355ced6cb2f0d9e92e727fa72
Koen van Haalen <32582137+vanhaalen@users.noreply.github.com>
2019-06-15 18:40:08 +0200

    Chapter 3: add tests to check score and auther (by) are rendered

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -12,6 +12,26 @@ describe('Item.vue', () => {
     expect(wrapper.text()).toContain(item.url)
   })
 
+  test('renders item.score', () => {
+    const item = {
+      score: 100
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+    expect(wrapper.text()).toContain(item.score)
+  })
+
+  test('renders item.by', () => {
+    const item = {
+      by: 'Koen'
+    }
+    const wrapper = shallowMount(Item, {
+      propsData: { item }
+    })
+    expect(wrapper.text()).toContain(item.by)
+  })
+
   test('renders a link to the item.url with item.title as text', () => {
     const item = {
       url: 'http://some-url.com',
