74ad1c11050245cedafb12ee1bfe76184795f535
Koen van Haalen <32582137+vanhaalen@users.noreply.github.com>
2019-06-15 18:24:08 +0200

    Chapter 3: Added styles and formating

--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="item-list">
-    <item v-for="item in displayItems" :key="item.id" :item="item"></item>
+  <div class="item-list-view">
+    <div class="item-list">
+      <item v-for="item in displayItems" :key="item.id" :item="item"></item>
+    </div>
   </div>
 </template>
 
@@ -19,3 +21,22 @@ export default {
   }
 }
 </script>
+
+<style>
+  .item-list-view {
+    padding-top: 45px;
+  }
+  .item-list {
+    background-color: #fff;
+    border-radius: 2px;
+    position: absolute;
+    margin: 30px 0;
+    width: 100%;
+    transition: all 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  }
+  @media (max-width: 600px) {
+    .item-list {
+      margin: 10px 0;
+    }
+  }
+</style>
